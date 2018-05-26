964973b47f40219fd47099e0859ec17c069377d3
Keir MacDonald <keir@kamd.me.uk>
2018-05-26 19:22:55 +0100

    More clean up.

--- EDScenicRouteWeb/EDScenicRouteWeb.Server/Controllers/ScenicSuggestionsController.cs
+++ EDScenicRouteWeb/EDScenicRouteWeb.Server/Controllers/ScenicSuggestionsController.cs
@@ -14,7 +14,6 @@ using Microsoft.Rest;
 
 namespace EDScenicRouteWeb.Server.Controllers
 {
-   // [Produces("application/json")]
     [Route("api/[controller]")]
     public class ScenicSuggestionsController : Controller
     {
@@ -26,18 +25,9 @@ namespace EDScenicRouteWeb.Server.Controllers
 
         private IGalaxyManager Galaxy { get; }
 
-        // GET: api/ScenicSuggestions
-     /*   [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-        */
         [HttpPost]
         public async Task<IActionResult> GetSuggestions([FromBody] RouteDetails details)
         {
-            //await Task.Delay(500);
-            //return new ScenicSuggestionResults() {StraightLineDistance = 123f, Suggestions = TestData};
             try
             {
                 var results = await Galaxy.GenerateSuggestions(details);
@@ -45,44 +35,9 @@ namespace EDScenicRouteWeb.Server.Controllers
             }
             catch (SystemNotFoundException systemNotFoundException)
             {
-                // throw error?
                 return NotFound($"System '{systemNotFoundException.SystemName}' was not found in the galaxy.");
-                
             }
-            
         }
-
-
-        public string Hello()
-        {
-            return "Why hello world!";
-        }
-        /*
-        // GET: api/ScenicSuggestions/5
-        [HttpGet("{id}", Name = "Get")]
-      //  [HttpGet()]
-        public string Get(int id)
-        {
-            return "value";
-        }
-        
-        // POST: api/ScenicSuggestions
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-        
-        // PUT: api/ScenicSuggestions/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-        
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }*/
 
         private List<ScenicSuggestion> TestData => new List<ScenicSuggestion>()
         {
