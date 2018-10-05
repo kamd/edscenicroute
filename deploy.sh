8bafdea3632462f4113f71113a4154571fd05661
Keir MacDonald <keir@kamd.me.uk>
2018-10-05 20:12:18 +0100

    Targetting release 2.1 SDK.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR
 pushd EDScenicRouteWeb/EDScenicRouteWeb.Server/
+rm -rf bin/Release
+mkdir -p bin/Release/netcoreapp2.1
+ln -s /var/www/elite bin/Release/netcoreapp2.1/publish
 dotnet publish -c Release -p:PublishWithAspNetCoreTargetManifest=false
 cp EDScenicRouteWeb.Client.blazor.config /var/www/elite/
 popd
